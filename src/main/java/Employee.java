d1deeb60824e5ba6908155d8948c2dec7ef636e4
Bethlehem <aberabetty3@gmail.com>
2025-10-21 22:35:35 -0400

    added the room that represent a hotel room and stores info about it. I created the reservation class that represents a reservation made by a guest. I created Employee.java class this class store and calculate payroll info for an Employee

---
+++ src/main/java/Employee.java
@@ -0,0 +1,46 @@
+public class Employee {
+    private int employedId;
+    private String name;
+    private String department;
+    private double payRate;
+    private double hoursWorked;
+
+    // costructor
+    public Employee(int employedId, String name, String department, double payRate, double hoursWorked ) {
+        this.employedId = employedId;
+        this.name = name;
+        this.department = department;
+        this.payRate = payRate;
+        this.hoursWorked = hoursWorked;
+    }
+    // getter
+    public int getEmployedId() {
+        return employedId;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDepartment() {
+        return department;
+    }
+    public double getPayRate() {
+        return payRate;
+    }
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+    // derived getter: calculates regular hours
+    public double getRegularHoursWorked() {
+        return Math.min(hoursWorked,40);
+    }
+    // derived getter overtime hours
+    public double getOvertimeHoursWorked() {
+        return Math.min(hoursWorked - 40, 0);
+    }
+    // derived getter total pay
+    public double getTotalPay() {
+        double regularPay = getRegularHoursWorked() * payRate;
+        double overtimePay = getOvertimeHoursWorked() * payRate * 1.5;
+        return regularPay + overtimePay;
+    }
+}
